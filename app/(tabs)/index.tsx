551c5d7d9914da4cec9acbdb4a15111212c013e8
uasr208 <arunsinghutkarsh10@gmail.com>
2026-01-27 21:27:36 +0530

    implemented fully functional trending movie section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,9 +16,17 @@ import { images } from "@/constants/images";
 
 import MovieCard from "@/component/MovieCard";
 import SearchBar from "@/component/SearchBar";
+import TrendingCard from "@/component/TrendingCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -41,14 +49,14 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
@@ -57,6 +65,25 @@ export default function Index() {
               }}
               placeholder="Search for a movie"
             />
+
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                />
+              </View>
+            )}
 
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
--- component/SearchBar.tsx
+++ component/SearchBar.tsx
@@ -4,8 +4,8 @@ import { Image, TextInput, View } from "react-native";
 interface props {
   placeholder: string;
   onPress?: () => void;
-  value: string;
-  onChangeText: (text: string) => void;
+  value?: string;
+  onChangeText?: (text: string) => void;
 }
 
 const SearchBar = ({ onPress, placeholder, value, onChangeText }: props) => {
